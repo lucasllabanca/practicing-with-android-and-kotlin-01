f25fb4defeb9c8de20915df1c23b9bc6f4032626
lucasllabanca <lucasllabanca@hotmail.com>
2022-04-30 15:04:18 -0300

    First app created and configured with text and view fields

--- app/src/main/java/br/com/labanca/androidproject01/MainActivity.kt
+++ app/src/main/java/br/com/labanca/androidproject01/MainActivity.kt
@@ -2,10 +2,26 @@ package br.com.labanca.androidproject01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import br.com.labanca.androidproject01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnSave.setOnClickListener {
+            Log.d("MainActivity", "Save button clicked")
+
+            binding.txtName.text = binding.edtName.text
+            binding.txtDescription.text = binding.edtDescription.text
+            binding.txtCode.text = binding.edtCode.text
+            binding.txtPrice.text = binding.edtPrice.text
+        }
     }
 }
